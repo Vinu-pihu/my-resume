fecc2496d54821fa12638ab48fcceff530e7723c
Virendra Kumar <141236959+Vinu-pihu@users.noreply.github.com>
2023-11-07 15:48:58 +0530

    added my-resume

---
+++ assets/js/typed-text.js
@@ -0,0 +1,1 @@
+$(document).ready(function($){'use strict';var typed=new Typed('#typed-text',{stringsElement:'#typed-strings',typeSpeed:60,backSpeed:30,startDelay:1000,loop:true,loopCount:Infinity});});
